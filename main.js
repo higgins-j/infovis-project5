f70926ebc74837e509ce708b8f53c2cad45ec6e4
morris88826 <mtseng9@gatech.edu>
2019-12-05 00:11:17 -0500

    polishing

--- main.js
+++ main.js
@@ -91,117 +91,145 @@ function brushMove(attribute) {
 }
 
 function brushEnd(attribute) {
-    var selection = d3.event.selection;
-    if (!selection) {
-        selectedChart = undefined;
-        svg.selectAll('.dot').classed('selected', false);
-		d3.select("#chartSpot").html("");
-    } else {
-		createPieChart(selection, attribute);
-	}
+  var selection = d3.event.selection;
+  if (!selection) {
+    selectedChart = undefined;
+    svg.selectAll(".dot").classed("selected", false);
+    d3.select("#chartSpot").html("");
+  } else {
+    createPieChart(selection, attribute);
+  }
 }
 
 function createPieChart(selection, attribute) {
-	d3.select("#chartSpot").html("");
-	var [[left, top], [right, bottom]] = selection;
-	var movies = new Array();
-	genreMap = new Map();
-	var dots = svg.selectAll('.dot').classed('selected', function(d) {
-		var x = xScale(d[attribute]) + chartPadding.l;
-		var y = yScale(d['gross']) + chartPadding.t;
-		if (left <= x && x <= right && top <= y && y <= bottom) {
-			//get values of all movies selected here
-			var contains = 0;
-			for (i = 0; i < movies.length; i++) {
-				if (movies[i].title == d.title) {
-					contains = 1;
-				}
-			}
-			if (contains == 0) {
-				movies.push(d);
-			}
-		}
-		return left <= x && x <= right && top <= y && y <= bottom;
-	});
-	for (i = 0; i < movies.length; i++) {
-		var splits = movies[i].genres.split('|');
-		for (j = 0; j < splits.length; j++) {
-			if (genreMap.has(splits[j])) {
-				var newNumber = genreMap.get(splits[j]) + 1;
-				genreMap.delete(splits[j]);
-				genreMap.set(splits[j], newNumber);
-			} else {
-				genreMap.set(splits[j], 1);
-			}
-		}
-	}
-
-    //word cloud
-    var word_dict = {};
-    for (i = 0; i < movies.length; i++) {
-      var words = movies[i]["plot_keywords"].split("|");
-      for (j = 0; j < words.length; j++) {
-        if (word_dict[words[j]] == undefined) {
-          word_dict[words[j]] = 1;
-        } else {
-          word_dict[words[j]] = word_dict[words[j]] + 1;
+  d3.select("#chartSpot").html("");
+  d3.select("#wordSpot").html("");
+  var [[left, top], [right, bottom]] = selection;
+  var movies = new Array();
+  genreMap = new Map();
+  var dots = svg.selectAll(".dot").classed("selected", function(d) {
+    var x = xScale(d[attribute]) + chartPadding.l;
+    var y = yScale(d["gross"]) + chartPadding.t;
+    if (left <= x && x <= right && top <= y && y <= bottom) {
+      //get values of all movies selected here
+      var contains = 0;
+      for (i = 0; i < movies.length; i++) {
+        if (movies[i].title == d.title) {
+          contains = 1;
         }
       }
-    }
-
-    var frequency_list = [];
-
-    for (var key in word_dict) {
-      if (word_dict.hasOwnProperty(key)) {
-        frequency_list.push({ text: key, size: word_dict[key] });
+      if (contains == 0) {
+        movies.push(d);
       }
     }
-    draw_word_cloud(frequency_list);
-    //end word cloud
+    return left <= x && x <= right && top <= y && y <= bottom;
+  });
+  for (i = 0; i < movies.length; i++) {
+    var splits = movies[i].genres.split("|");
+    for (j = 0; j < splits.length; j++) {
+      if (genreMap.has(splits[j])) {
+        var newNumber = genreMap.get(splits[j]) + 1;
+        genreMap.delete(splits[j]);
+        genreMap.set(splits[j], newNumber);
+      } else {
+        genreMap.set(splits[j], 1);
+      }
+    }
+  }
 
-	var pieWidth = 500;
-	var pieHeight = 500;
-	var pieRadius = 225;
-	chartSpot = d3.select("#chartSpot").append("svg")
-			.attr("width", pieWidth)
-			.attr("height", pieHeight)
-			.append("g")
-			.attr("transform", "translate(" + pieWidth / 2 + "," + pieHeight / 2 + ")");
-	var color = d3.scaleOrdinal(d3.schemeSet2);
-	var pie = d3.pie();
-	var dataReady = pie(Array.from(genreMap.values()));
-	console.log(dataReady);
-	var arcGenerator = d3.arc()
-		.innerRadius(0)
-		.outerRadius(pieRadius)
+  //word cloud
+  var word_dict = {};
+  for (i = 0; i < movies.length; i++) {
+    var words = movies[i]["plot_keywords"].split("|");
+    for (j = 0; j < words.length; j++) {
+      if (word_dict[words[j]] == undefined) {
+        word_dict[words[j]] = 1;
+      } else {
+        word_dict[words[j]] = word_dict[words[j]] + 1;
+      }
+    }
+  }
 
-	chartSpot.selectAll('arc')
-		.data(dataReady)
-		.enter()
-		.append('path')
-			.attr('d', arcGenerator)
-			.attr('fill', function(d,i) {
-				return color(i);
-			})
-			.style("stroke", "black")
-			.style("stroke-width", "2px");
+  var sorted = Object.keys(word_dict).map(function(key) {
+    return [key, word_dict[key]];
+  });
 
-	chartSpot.selectAll('arc')
-		.data(dataReady)
-		.enter()
-		.append('text')
-		.text(function(d,i) {
-			return Array.from(genreMap.keys())[i];
-		})
-		.each(function(d, i) {
-			var centroid = arcGenerator.centroid(d);
-			d3.select(this)
-				.attr('x', centroid[0])
-				.attr('y', centroid[1])
-				.attr('dy', '0.33em');
-		})
-		.style("text-anchor", "middle")
-		.style("font-size", 12);
+  // Sort the array based on the second element
+  sorted.sort(function(first, second) {
+    return second[1] - first[1];
+  });
+
+  var slice_idx = sorted.length > 30 ? 30 : sorted.length;
+  sorted = sorted.slice(0, slice_idx);
+  word_scale = 0;
+  var frequency_list = [];
+  for (var i = 0; i < sorted.length; i++) {
+    word_scale += sorted[i][1] / sorted.length;
+  }
+  word_scale = 30 / word_scale;
+
+  for (var i = 0; i < sorted.length; i++) {
+    frequency_list.push({
+      text: sorted[i][0],
+      size: sorted[i][1] * word_scale
+    });
+  }
+
+  draw_word_cloud(frequency_list);
+  //end word cloud
+
+  var pieWidth = 500;
+  var pieHeight = 500;
+  var pieRadius = 225;
+  chartSpot = d3
+    .select("#chartSpot")
+    .append("svg")
+    .attr("width", pieWidth)
+    .attr("height", pieHeight)
+    .append("g")
+    .attr("transform", "translate(" + pieWidth / 2 + "," + pieHeight / 2 + ")");
+  var color = d3.scaleOrdinal(d3.schemeSet2);
+  var pie = d3.pie();
+  var dataReady = pie(Array.from(genreMap.values()));
+  var avgChart = 0;
+  for (var i = 0; i < dataReady.length; i++) {
+    avgChart += dataReady[i]["data"] / dataReady.length;
+  }
+
+  var arcGenerator = d3
+    .arc()
+    .innerRadius(0)
+    .outerRadius(pieRadius);
+
+  chartSpot
+    .selectAll("arc")
+    .data(dataReady)
+    .enter()
+    .append("path")
+    .attr("d", arcGenerator)
+    .attr("fill", function(d, i) {
+      return color(i);
+    })
+    .style("stroke", "black")
+    .style("stroke-width", "2px");
+
+  chartSpot
+    .selectAll("arc")
+    .data(dataReady)
+    .enter()
+    .append("text")
+    .text(function(d, i) {
+      return d["data"] > avgChart * 0.4 ? Array.from(genreMap.keys())[i] : "";
+    })
+    .each(function(d, i) {
+      var centroid = arcGenerator.centroid(d);
+      d3.select(this)
+        .attr("x", centroid[0])
+        .attr("y", centroid[1])
+        .attr("dy", "0.33em");
+    })
+    .style("text-anchor", "middle")
+    .style("font-size", 12);
 }
 
 d3.csv("movies.csv", function(csv) {
--- word_cloud.js
+++ word_cloud.js
@@ -1,20 +1,56 @@
-var color = d3
-  .scaleLinear()
-  .domain([0, 1, 2, 3, 4, 5, 6, 10, 15, 20, 100])
-  .range([
-    "#ddd",
-    "#ccc",
-    "#bbb",
-    "#aaa",
-    "#999",
-    "#888",
-    "#777",
-    "#666",
-    "#555",
-    "#444",
-    "#333",
-    "#222"
-  ]);
+var colorArray = [
+  "#FF6633",
+  "#FFB399",
+  "#FF33FF",
+  "#FFFF99",
+  "#00B3E6",
+  "#E6B333",
+  "#3366E6",
+  "#999966",
+  "#99FF99",
+  "#B34D4D",
+  "#80B300",
+  "#809900",
+  "#E6B3B3",
+  "#6680B3",
+  "#66991A",
+  "#FF99E6",
+  "#CCFF1A",
+  "#FF1A66",
+  "#E6331A",
+  "#33FFCC",
+  "#66994D",
+  "#B366CC",
+  "#4D8000",
+  "#B33300",
+  "#CC80CC",
+  "#66664D",
+  "#991AFF",
+  "#E666FF",
+  "#4DB3FF",
+  "#1AB399",
+  "#E666B3",
+  "#33991A",
+  "#CC9999",
+  "#B3B31A",
+  "#00E680",
+  "#4D8066",
+  "#809980",
+  "#E6FF80",
+  "#1AFF33",
+  "#999933",
+  "#FF3380",
+  "#CCCC00",
+  "#66E64D",
+  "#4D80CC",
+  "#9900B3",
+  "#E64D66",
+  "#4DB380",
+  "#FF4D4D",
+  "#99E6E6",
+  "#6666FF"
+];
+
 function draw_word_cloud(frequency_list) {
   console.log("draw");
   d3.layout
@@ -47,7 +83,7 @@ function draw(words) {
       return d.size + "px";
     })
     .style("fill", function(d, i) {
-      return color(i);
+      return colorArray[i];
     })
     .attr("transform", function(d) {
       return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
